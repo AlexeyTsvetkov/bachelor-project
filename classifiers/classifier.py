a8299a4aed192145850a5206cc89dfdcc310484c
Alexey Tsvetkov <type.inference@gmail.com>
2013-05-26 17:32:48 +0700

    Moved get_encoded_labels(), classify_one() up to ClassifierBase

--- classifiers/classifier.py
+++ classifiers/classifier.py
@@ -11,6 +11,23 @@ class ClassifierBase(object):
     def __str__(self):
         return 'BaseClassifier'
 
+    def get_encoded_labels(self, labels):
+        """Encodes each label to unique index in 0..unique_class_count"""
+
+        self.class_index = {}
+        self.classes = []
+        self.class_probability = []
+
+        encoded_labels = np.zeros((len(labels),))
+        for i in xrange(len(labels)):
+            label = labels[i]
+            if label not in self.class_index:
+                self.class_index[label] = len(self.classes)
+                self.classes.append(label)
+            encoded_labels[i] = self.class_index[label]
+
+        return encoded_labels
+
     def learn(self, train_set, classes):
         """Learns from train_set
         train_set -- list of documents
@@ -19,11 +36,16 @@ class ClassifierBase(object):
         """
         raise NotImplementedError('ClassifierBase.learn')
 
-    def classify_one(self, text):
-        """Classifies text document
-        returns document class
-        """
-        raise NotImplementedError('ClassifierBase.classify')
+    def conditional_probability(self, Class, document):
+        """Probability that document belongs to Class"""
+        raise NotImplementedError('ClassifierBase.conditional_probability')
+
+    def classify_one(self, document):
+        document = self.preprocessor.preprocess(document)
+        classes = range(len(self.classes))
+        prob_of_class = lambda c: self.conditional_probability(c, document)
+
+        return max(classes, key=prob_of_class)
 
     def classify_batch(self, text_set, original_class=False):
         """Classifies each text document in text_set
@@ -65,32 +87,21 @@ class MultinomialNaiveBayes(ClassifierBase):
     def get_class_count(self):
         return len(self.classes)
 
-    def get_encoded_labels(self, labels):
-        """Encodes each label to unique index in 0..unique_class_count"""
-
-        self.class_index = {}
-        self.classes = []
-        self.class_probability = []
-
-        encoded_labels = []
-        class_document_count = []
+    def get_class_prob(self, labels):
+        class_count = len(self.classes)
+        class_document_count = np.zeros(class_count)
         document_count = len(labels)
-        for c in labels:
-            if c not in self.class_index:
-                self.class_index[c] = self.get_class_count()
-                class_document_count.append(0)
-                self.classes.append(c)
-            class_document_count[self.class_index[c]] += 1
-            encoded_labels.append(self.class_index[c])
 
-        for Class in xrange(self.get_class_count()):
-            self.class_probability.append(1.0 * class_document_count[Class] / document_count)
+        for label in labels:
+            class_document_count[label] += 1
 
-        return encoded_labels
+        return class_document_count / document_count
 
     def learn(self, documents, labels):
         documents = map(self.preprocessor.preprocess, documents)
         labels = self.get_encoded_labels(labels)
+
+        self.class_probability = self.get_class_prob(labels)
 
         self.feature_extractor.learn(documents, labels)
         self.feature_count = self.feature_extractor.features_count()
@@ -107,25 +118,12 @@ class MultinomialNaiveBayes(ClassifierBase):
         for Class in xrange(self.get_class_count()):
             self.class_feature_sum.append(np.sum(self.features_counts[Class]))
 
-    def classify_one(self, document):
-        result_class = -1
-        max_posterior = None
-        alpha = self.alpha
-
-        document = self.preprocessor.preprocess(document)
+    def conditional_probability(self, Class, document):
+        """Probability that document belongs to Class"""
         feature_vector = self.feature_extractor.extract(document)
-        for Class in xrange(self.get_class_count()):
-            prior = math.log(self.class_probability[Class], 10)
-            likelihood = (self.features_counts[Class] + alpha) /\
-                         (self.features_counts[Class] + self.feature_count * alpha)
-            log_likelihood = np.sum(feature_vector * np.log10(likelihood))
-            posterior = prior + log_likelihood
-            if not max_posterior or posterior >= max_posterior:
-                result_class = Class
-                max_posterior = posterior
-
-        if result_class == -1:
-            raise RuntimeError('Result class is negative for text "%s"'
-                               % (document,))
-
-        return result_class
+        prior = math.log(self.class_probability[Class], 10)
+        likelihood = (self.features_counts[Class] + self.alpha) / \
+                     (self.features_counts[Class] + self.feature_count * self.alpha)
+        log_likelihood = np.sum(feature_vector * np.log10(likelihood))
+        posterior = prior + log_likelihood
+        return posterior
